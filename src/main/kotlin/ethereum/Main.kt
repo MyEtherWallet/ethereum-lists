755c5c762297047512d8df12dc6ef48e1239109f
ligi <ligi@ligi.de>
2017-08-26 19:11:53 +0200

    Create minified version of JSON-files (no optional fields)

--- src/main/kotlin/ethereum/Main.kt
+++ src/main/kotlin/ethereum/Main.kt
@@ -20,25 +20,27 @@ fun main(args: Array<String>) {
 
 }
 
-fun check(directoryName: String, checkFunction: (array: JsonArray<*>) -> Unit) {
-    File(directoryName).listFiles { _, name -> name.endsWith(".json") }.forEach { it ->
-        println("processing $directoryName $it")
-        checkFunction(Parser().parse(it.reader()) as JsonArray<*>)
+fun check(directoryName: String, checkFunction: (file: File) -> Unit) {
+    File(directoryName).listFiles { _, name -> name.endsWith(".json") }.forEach { jsonFile ->
+        println("processing $directoryName $jsonFile")
+        checkFunction(jsonFile)
     }
 }
 
 val EMAIL_REGEX = Regex("(?:[a-z0-9!#\$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#\$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
 
-fun JsonArray<*>.checkEmails() {
-    map { it as String }.forEach { email ->
+fun File.toJSONArray() = Parser().parse(this.reader()) as JsonArray<*>
+fun File.checkEmails() {
+    toJSONArray().map { it as String }.forEach { email ->
         if (!EMAIL_REGEX.matches(email)) {
             throw IllegalArgumentException("invalid email $email")
         }
     }
 }
 
-fun JsonArray<*>.checkFields(mandatoryFields: List<String>, optionalFields: List<String> = listOf()) {
-    map { it as JsonObject }.forEach { jsonObject ->
+fun File.checkFields(mandatoryFields: List<String>, optionalFields: List<String> = listOf()) {
+    val jsonObjectList = toJSONArray().map { it as JsonObject }
+    jsonObjectList.forEach { jsonObject ->
         if (!jsonObject.keys.containsAll(mandatoryFields)) {
             throw IllegalArgumentException("$jsonObject does not contain " + mandatoryFields.minus(jsonObject.keys))
         }
@@ -47,5 +49,19 @@ fun JsonArray<*>.checkFields(mandatoryFields: List<String>, optionalFields: List
         if (unknownFields.isNotEmpty()) {
             throw IllegalArgumentException("$jsonObject contains unknown " + unknownFields)
         }
+
+    }
+    if (!optionalFields.isEmpty()) {
+        val minimalJSONArray = JsonArray<JsonObject>()
+        jsonObjectList.forEach { jsonObject ->
+            val minimalJsonObject = JsonObject()
+            mandatoryFields.forEach {
+                minimalJsonObject.put(it, jsonObject[it])
+            }
+            minimalJSONArray.add(minimalJsonObject)
+        }
+
+        File("build/output").mkdir()
+        File("build/output", name.replace(".json", ".min.json")).writeText(minimalJSONArray.toJsonString())
     }
 }
