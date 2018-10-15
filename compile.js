00ba0fa67f53baf294535ce754c3c6f695bdb25a
gamalielhere <gamalielgad1234@gmail.com>
2018-10-15 14:56:28 -0700

    Recompile, try catch to catch errors instead of proceeding;

--- compile.js
+++ compile.js
@@ -6,20 +6,25 @@ const recreateFiles = require('./recreateFiles');
 const renameToChecksum = require('./renameToChecksum');
 
 function compile() {
-  console.log('Start');
-  createFiles();
-  console.log('Created files!');
-  checkDirectory();
-  console.log('Directory is clean!');
-  recreateFiles();
-  console.log('Recreated files!');
-  renameToChecksum();
-  console.log('Rename files to checksum!');
-  checkContract();
-  console.log('Contracts are fine!');
-  checkToken();
-  console.log('Tokens are fine!');
-  console.log('Done');
+  try {
+    console.log('Start');
+    createFiles();
+    console.log('Created files!');
+    checkDirectory();
+    console.log('Directory is clean!');
+    recreateFiles();
+    console.log('Recreated files!');
+    renameToChecksum();
+    console.log('Rename files to checksum!');
+    checkContract();
+    console.log('Contracts are fine!');
+    checkToken();
+    console.log('Tokens are fine!');
+    console.log('Done');
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
 }
 
 compile();
