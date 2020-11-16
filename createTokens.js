3e4960784c97762eba7d0b83724e59c2b2543360
gamalielhere <gamalielgad1234@gmail.com>
2020-11-16 14:33:57 -0800

    generate more tokens

--- createTokens.js
+++ createTokens.js
@@ -5,86 +5,115 @@ const utils = Web3.utils;
 const tokensDirectory = './src/tokens/eth/';
 const notInListPath = './notinlist.json';
 const notInList = JSON.parse(fs.readFileSync(notInListPath));
-const api = 'https://api.coingecko.com/api/v3/coins/ethereum/contract'; 
+const api = 'https://api.coingecko.com/api/v3/coins/ethereum/contract';
 const node = 'https://nodes.mewapi.io/rpc/eth';
 const abi = [
   {
-      "constant": true,
-      "inputs": [],
-      "name": "decimals",
-      "outputs": [
-          {
-              "name": "",
-              "type": "uint8"
-          }
-      ],
-      "payable": false,
-      "stateMutability": "view",
-      "type": "function"
-  }];
+    constant: true,
+    inputs: [],
+    name: 'decimals',
+    outputs: [
+      {
+        name: '',
+        type: 'uint8'
+      }
+    ],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
+  }
+];
 const web3 = new Web3(node);
 async function createToken() {
   for (let index = 0; index < notInList.length; index++) {
     try {
       const contract = new web3.eth.Contract(abi, notInList[index]);
-      const decimal = await contract.methods.decimals().call().catch(res => {
-        console.log(notInList[index], res)
-      });
-      const tokenInfo = await fetch(`${api}/${notInList[index]}`).then(response => {
-        return response.json();
-      });
-      if(!tokenInfo.hasOwnProperty('error')) {
-        const homepage = tokenInfo.hasOwnProperty('links') ? tokenInfo.links.hasOwnProperty('homepage') ? tokenInfo.links.homepage[0] : '' : '';
-        const tokenTemp =
-            {
-              "symbol": tokenInfo.symbol.toUpperCase(),
-              "name": tokenInfo.name,
-              "type": "ERC20",
-              "address": utils.toChecksumAddress(notInList[index]),
-              "ens_address": "",
-              "decimals": Number(decimal),
-              "website": homepage,
-              "logo": {
-                  "src": "",
-                  "width": "",
-                  "height": "",
-                  "ipfs_hash": ""
-              },
-              "support": {
-                  "email": "",
-                  "url": ""
-              },
-              "social": {
-                  "blog": "",
-                  "chat": "",
-                  "discord": "",
-                  "facebook": "",
-                  "forum": "",
-                  "github": "",
-                  "gitter": "",
-                  "instagram": "",
-                  "linkedin": "",
-                  "reddit": "",
-                  "slack": "",
-                  "telegram": "",
-                  "twitter": "",
-                  "youtube": ""
-              }
-          };
-            fs.writeFileSync(
-              `${tokensDirectory}/${utils.toChecksumAddress(
-                notInList[index].replace('.json', '')
-              )}.json`,
-              JSON.stringify(tokenTemp)
-
-              );
+      const decimal = await contract.methods
+        .decimals()
+        .call()
+        .catch(() => {
+          console.log(`Could not fetch decimal for: ${notInList[index]}!`);
+        });
+      const tokenInfo = await fetch(`${api}/${notInList[index]}`).then(
+        response => {
+          return response.json();
+        }
+      );
+      const tokenTemp = {
+        symbol: '',
+        name: '',
+        type: 'ERC20',
+        address: '',
+        ens_address: '',
+        decimals: 0,
+        website: '',
+        logo: {
+          src: '',
+          width: '',
+          height: '',
+          ipfs_hash: ''
+        },
+        support: {
+          email: '',
+          url: ''
+        },
+        social: {
+          blog: '',
+          chat: '',
+          discord: '',
+          facebook: '',
+          forum: '',
+          github: '',
+          gitter: '',
+          instagram: '',
+          linkedin: '',
+          reddit: '',
+          slack: '',
+          telegram: '',
+          twitter: '',
+          youtube: ''
+        }
+      };
+      if (!tokenInfo.hasOwnProperty('error')) {
+        const homepage = tokenInfo.hasOwnProperty('links')
+          ? tokenInfo.links.hasOwnProperty('homepage')
+            ? tokenInfo.links.homepage[0]
+            : ''
+          : '';
+        const newTokenCopy = Object.assign({}, tokenTemp, {
+          symbol: tokenInfo.symbol.toUpperCase(),
+          name: tokenInfo.name,
+          address: utils.toChecksumAddress(notInList[index]),
+          decimals: Number(decimals),
+          website: homepage
+        });
+        fs.writeFileSync(
+          `${tokensDirectory}/${utils.toChecksumAddress(
+            notInList[index].replace('.json', '')
+          )}.json`,
+          JSON.stringify(newTokenCopy)
+        );
       } else {
-        console.log(`CoinGecko could not find ${notInlist[index]}`)
+        const newTokenCopy = Object.assign({}, tokenTemp, {
+          symbol: '',
+          name: '',
+          address: utils.toChecksumAddress(notInList[index]),
+          decimals: Number(decimals),
+          website: ''
+        });
+        fs.writeFileSync(
+          `${tokensDirectory}/${utils.toChecksumAddress(
+            notInList[index].replace('.json', '')
+          )}.json`,
+          JSON.stringify(newTokenCopy)
+        );
+        console.log(
+          `CoinGecko could not find ${notInList[index]}! Some info will be missing`
+        );
       }
-    } catch(e) {
-      console.log(e, notInList[index])
+    } catch (e) {
+      console.log(e, notInList[index]);
     }
   }
-  
 }
 createToken();
