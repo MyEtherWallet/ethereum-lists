75bef5c97e07f4d15af23b537da48b05c79df761
gamalielhere <gamalielgad1234@gmail.com>
2021-01-25 10:52:26 -0800

    recompile

--- generateMissingTokenListFromIcons.js
+++ generateMissingTokenListFromIcons.js
@@ -10,38 +10,48 @@ function generateMissingToken() {
   const exclusion = [
     '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
     '0xef68e7c694f40c8202821edf525de3782458639f',
-    '0x85e076361cc813a908ff672f9bad1541474402b2' // TEL token migrated
-  ]
+    '0x85e076361cc813a908ff672f9bad1541474402b2', // TEL token migrated
+    '0xd4260e4Bfb354259F5e30279cb0D7F784Ea5f37A' // contract getting included from icons
+  ];
 
   const addressOnly = [];
   const notInList = [];
   icons.forEach(icon => {
     const idxOf = icon.indexOf('-0x');
-    const getAddr = icon.substring(idxOf+1, icon.length);
-    if(getAddr.length !== 42) {
+    const getAddr = icon.substring(idxOf + 1, icon.length);
+    if (getAddr.length !== 42) {
       const actualAddress = getAddr.substring(getAddr.indexOf('0x'), 42);
       addressOnly.push(actualAddress);
     } else {
       addressOnly.push(getAddr);
     }
-  })
-  
+  });
+
   addressOnly.forEach(addr => {
-    if(utils.isAddress(addr)) {
+    if (utils.isAddress(addr)) {
       const inExclusionList = exclusion.find(item => {
         return utils.toChecksumAddress(item) === utils.toChecksumAddress(addr);
-      })
+      });
       const found = list.find(item => {
-        if(addr.substring(0, 2) === '0x' && addr.length === 42) {
-          return utils.toChecksumAddress(item.address) === utils.toChecksumAddress(addr);
+        if (addr.substring(0, 2) === '0x' && addr.length === 42) {
+          return (
+            utils.toChecksumAddress(item.address) ===
+            utils.toChecksumAddress(addr)
+          );
         }
-      })
-      if(!found && addr.substring(0, 2) === '0x' && addr.length === 42 && !inExclusionList) notInList.push(addr);
+      });
+      if (
+        !found &&
+        addr.substring(0, 2) === '0x' &&
+        addr.length === 42 &&
+        !inExclusionList
+      )
+        notInList.push(addr);
     } else {
       console.log(addr);
     }
   });
-  fs.writeFileSync('notinlist.json', JSON.stringify(notInList))
+  fs.writeFileSync('notinlist.json', JSON.stringify(notInList));
 }
 
 generateMissingToken();
