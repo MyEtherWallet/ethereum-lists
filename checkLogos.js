bfd01f8d5dd05169b3ae844e85da208f9faa8d9f
David Hong <hsurf22@gmail.com>
2021-09-17 16:38:00 -0700

    update download function and rename dirs

--- checkLogos.js
+++ checkLogos.js
@@ -1,31 +1,89 @@
+const axios = require('axios');
 const fs = require('fs');
-const tokensDirectory = './src/tokens';
-const request = require('request');
+const URL = require('url').URL;
 
-function checkTokenLogos() {
-  let invalidImgArray = [];
-  fs.readdirSync(tokensDirectory).forEach(folder => {
-    fs.readdirSync(`${tokensDirectory}/${folder}`).forEach(file => {
-      const fullPath = `${tokensDirectory}/${folder}/${file}`;
-      const obj = JSON.parse(fs.readFileSync(fullPath, 'utf8'));
+// Base tokens dir
+const tokensBaseDirectory = './src/tokens';
 
-      if (obj.logo.src === '') {
-        return;
+// Save dir
+const saveDirectory = './src/new-icons';
+
+// Get all sub dirs under base token dir
+const getSubDirectories = source =>
+  fs
+    .readdirSync(source, { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => dirent.name);
+
+// Get token image URL if available
+const getTokenLogoURL = filePath => {
+  const rawdata = fs.readFileSync(filePath);
+  const token = JSON.parse(rawdata);
+
+  if (token.logo.src) {
+    return token.logo.src;
+  }
+  return false;
+};
+
+// Verify token image URL
+const stringIsAValidUrl = s => {
+  try {
+    new URL(s);
+    return true;
+  } catch (err) {
+    return false;
+  }
+};
+
+// Download token image and save
+const downloadLogo = logoURL => {
+  const localFilename = logoURL.substring(logoURL.lastIndexOf('/') + 1);
+  const localFileFullPath = `${saveDirectory}/${localFilename}`;
+
+  axios({
+    method: 'get',
+    url: logoURL,
+    responseType: 'stream',
+    timeout: 5000
+  }).then(
+    function(response) {
+      console.log(`[SUCCESS]: ${logoURL}`);
+      response.data.pipe(fs.createWriteStream(localFileFullPath));
+    },
+    error => {
+      console.log(`[😔 FAILED]: ${logoURL}`);
+      //console.log(error);
+    }
+  );
+};
+
+const run = () => {
+  // Get list of sub dirs
+  const subDirs = getSubDirectories(tokensBaseDirectory);
+
+  subDirs.forEach(subDir => {
+    // Full path of sub dir
+    const tokenDirPath = `${tokensBaseDirectory}/${subDir}`;
+
+    // Scan for files in sub dir
+    fs.readdir(tokenDirPath, (err, files) => {
+      if (err) {
+        console.log('Unable to scan directory: ' + err);
       }
 
-      request({method: 'HEAD', uri: obj.logo.src}, function(err, response) {
-        if (err) {
-          invalidImgArray.push(obj.logo.src);
-        }
+      files.forEach(async file => {
+        const tokenFilePath = `${tokensBaseDirectory}/${subDir}/${file}`;
+        const tokenLogoLink = getTokenLogoURL(tokenFilePath);
 
-        if (!err && response.statusCode === 400) {
-          invalidImgArray.push(obj.logo.src);
-        }
-        
-        fs.writeFileSync('./invalidLogoSrc.js', JSON.stringify(invalidImgArray))
-      })
-    }) 
-  })
-}
+        const isValidURL = stringIsAValidUrl(tokenLogoLink);
 
-checkTokenLogos();
+        if (tokenLogoLink && isValidURL) {
+          await downloadLogo(tokenLogoLink);
+        }
+      });
+    });
+  });
+};
+
+run();
