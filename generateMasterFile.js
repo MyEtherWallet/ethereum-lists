7df0b6595e242aeb826aa803deea96274a923072
Gamaliel Padillo <gamalielgad1234@gmail.com>
2025-10-20 13:55:49 -0700

    chore: revert sol changes

--- generateMasterFile.js
+++ generateMasterFile.js
@@ -55,54 +55,68 @@ function generateMasterFile() {
       });
     }
   });
-  [...Object.keys(imageCache.png), ...Object.keys(imageCache.svg)].forEach(
-    addr => {
-      const split = addr.split('-');
-      if (split.length < 2) return;
-      const actualAddress = split[1].substring(0, split[1].length - 4);
+  // [...Object.keys(imageCache.png), ...Object.keys(imageCache.svg)].forEach(
+  //   addr => {
+  //     const split = addr.split('-');
+  //     if (split.length < 2) return;
+  //     const actualAddress = split[1].substring(0, split[1].length - 4);
 
-      // non-evm address
-      if (!isAddress(actualAddress)) {
-        const isSol = /^[1-9A-HJ-NP-Za-km-z]{32,44}$/.test(actualAddress);
-        const isDot = /^1[a-zA-Z0-9]{47}$/.test(actualAddress);
+  //     // non-evm address
+  //     if (!isAddress(actualAddress)) {
+  //       const isSol = /^[1-9A-HJ-NP-Za-km-z]{32,44}$/.test(actualAddress);
+  //       const isDot = /^1[a-zA-Z0-9]{47}$/.test(actualAddress);
 
-        const symbol = split[0];
-        const mainURL =
-          'https://raw.githubusercontent.com/MyEtherWallet/ethereum-lists/master/src/icons/';
+  //       const symbol = split[0];
+  //       const mainURL =
+  //         'https://raw.githubusercontent.com/MyEtherWallet/ethereum-lists/master/src/icons/';
 
-        if (actualAddress.length < 32) return;
-        mainArr.push({
-          network: isSol ? 'sol' : isDot ? 'dot' : 'sol',
-          symbol: symbol,
-          name: symbol,
-          decimals: 0,
-          contract_address: actualAddress,
-          icon: `${
-            imageCache.png[addr]
-              ? imageCache.png[addr]
-              : imageCache.svg[addr]
-              ? imageCache.svg[addr]
-              : ''
-          }`,
-          icon_png: `${
-            imageCache.png[addr]
-              ? imageCache.png[addr]
-              : imageCache.svg[addr]
-              ? imageCache.svg[addr]
-              : ''
-          }`,
-          link: `${
-            imageCache.png[addr]
-              ? imageCache.png[addr]
-              : imageCache.svg[addr]
-              ? imageCache.svg[addr]
-              : ''
-          }`,
-          website: ''
-        });
-      }
-    }
-  );
+  //       if (actualAddress.length < 32) return;
+  //       mainArr.push({
+  //         network: isSol ? 'sol' : isDot ? 'dot' : 'sol',
+  //         symbol: symbol,
+  //         name: symbol,
+  //         decimals: 0,
+  //         contract_address: actualAddress,
+  //         icon: `${
+  //           imageCache.png[addr]
+  //             ? imageCache.png[addr]
+  //             : imageCache.svg[addr]
+  //             ? imageCache.svg[addr]
+  //             : ''
+  //         }`,
+  //         icon_png: `${
+  //           imageCache.png[addr]
+  //             ? imageCache.png[addr]
+  //             : imageCache.svg[addr]
+  //             ? imageCache.svg[addr]
+  //             : ''
+  //         }`,
+  //         link: `${
+  //           imageCache.png[addr]
+  //             ? imageCache.png[addr]
+  //             : imageCache.svg[addr]
+  //             ? imageCache.svg[addr]
+  //             : ''
+  //         }`,
+  //         website: ''
+  //       });
+  //     }
+  //   }
+  // );
+  // mainArr.push({
+  //   network: 'sol',
+  //   symbol: 'SOL',
+  //   name: 'Solana',
+  //   decimals: 0,
+  //   contract_address: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
+  //   icon:
+  //     'https://raw.githubusercontent.com/MyEtherWallet/ethereum-lists/master/src/icons/SOL-0x570A5D26f7765Ecb712C0924E4De545B89fD43dF.png',
+  //   icon_png:
+  //     'https://raw.githubusercontent.com/MyEtherWallet/ethereum-lists/master/src/icons/SOL-0x570A5D26f7765Ecb712C0924E4De545B89fD43dF.png',
+  //   link:
+  //     'https://raw.githubusercontent.com/MyEtherWallet/ethereum-lists/master/src/icons/SOL-0x570A5D26f7765Ecb712C0924E4De545B89fD43dF.png',
+  //   website: ''
+  // });
   mainArr.push({
     network: 'btc',
     symbol: 'BTC',
